fc709e8b48c10ef15b09100ba4ee4760c83a0a00
Adrian <adrianpatrykstadnicki@gmail.com>
2019-05-09 15:12:18 +0200

    init

---
+++ js/Hello.js
@@ -0,0 +1,24 @@
+const output = document.querySelector('#message');
+const input = document.querySelector('#seconds');
+
+const popularWords = [
+    'the',
+    'be',
+    'to',
+    'of',
+    'and',
+    'in',
+    'that',
+    'have',
+    'it',
+    'for',
+    'not',
+    'on',
+    'with',
+    'he',
+    'as',
+    'you',
+    'do',
+    'at'
+  ];
+
