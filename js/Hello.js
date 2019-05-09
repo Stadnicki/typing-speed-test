ffdd69ffe4c6ae35059c42573dcc3b5668803ce6
Adrian <adrianpatrykstadnicki@gmail.com>
2019-05-09 17:57:00 +0200

    counting correctly entered words

--- js/Hello.js
+++ js/Hello.js
@@ -1,7 +1,13 @@
 window.addEventListener('load', init);
 
 var output = document.getElementById('words-output');
-var input = document.getElementById('words-input"');
+var input = document.getElementById('words-input');
+var timerClock = document.getElementById('timer')
+
+var currentWord = ""
+var currentlyEntered = ""
+var isGameStarted = false;
+var counter = 0;
 
 const popularWords = [
     'the',
@@ -24,7 +30,24 @@ const popularWords = [
     'at'
   ];
 
-  var gameWords = []
+var gameWords = []
+
+function checkTimer() {
+  if(!isGameStarted){
+    isGameStarted = true;
+    var seconds = 60;
+    setInterval(function () {
+      timerClock.textContent = seconds;
+      seconds--;
+    }, 1000);
+  }
+  else if(seconds === 0){
+    isGameStarted = false;
+    seconds = 60;
+    return false;
+  }
+  return true;
+}
 
 
 function init() {
@@ -39,4 +62,46 @@ function loadWords(wordsArray){
     outputString += '<span>'+word+'</span>'
   }
   output.innerHTML = outputString
+  output.firstElementChild.classList.add("current-word");
+  currentWord = output.firstChild.textContent
+  console.log("current start: " + currentWord)
+}
+
+function handleKeyPressed(e){ 
+  keynum = e.keyCode;
+
+  checkTimer();
+
+  if(currentlyEntered === "" && keynum === 32){
+    console.log("empty")
+    input.value = "";
+  }
+  else if (keynum === 32){
+    input.value = "";
+    console.log("checking word")
+    checkWord(currentlyEntered);
+    currentlyEntered = ""
+  }
+  else if(keynum === 8){
+    currentlyEntered = currentlyEntered.substring(0, currentlyEntered.length-1);
+  }
+  else {
+    currentlyEntered += String.fromCharCode(keynum);
+  }
+}
+
+function checkWord(enteredWord){
+  console.log("words:(" + enteredWord + "|" + currentWord+")")
+  if(enteredWord.toLowerCase() === currentWord.toLowerCase()){
+    console.log("match")
+    counter++;
+  }
+  let current = document.querySelector(".current-word");
+  current.nextSibling.classList.add("current-word")
+  current.classList.remove("current-word")
+  current = document.querySelector(".current-word");
+  currentWord = current.textContent;
+  console.log("new current word: " + currentWord)
+  document.getElementById('correct-counter').innerHTML =counter
+  
 }
